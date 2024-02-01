7d95902c1302aa371f96e6568c38d379301dc614
Téo <teo.villet2@gmail.com>
2024-02-01 23:39:52 +0100

    add differents colors and make cube flying around the tower(i want to kill myself)

--- f-henge.js
+++ f-henge.js
@@ -63,23 +63,65 @@ const modelViewMatrixLocation = gl.getUniformLocation(prg, 'u_modelViewMatrix')
 const vao = gl.createVertexArray()
 gl.bindVertexArray(vao)
 
-const positionBuffer = gl.createBuffer()
-gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer)
+//For cube geometry
+
+// For ground geometry
+const positionGroundBuffer = gl.createBuffer()
+gl.bindBuffer(gl.ARRAY_BUFFER, positionGroundBuffer)
+gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(getGroundGeometry()), gl.STATIC_DRAW)
+gl.enableVertexAttribArray(positionLocation)
+gl.vertexAttribPointer(positionLocation, 3, gl.FLOAT, false, 0, 0)
+
+const colorGroundBuffer = gl.createBuffer()
+gl.bindBuffer(gl.ARRAY_BUFFER, colorGroundBuffer)
+setColors(gl, 6, 18)
+gl.enableVertexAttribArray(colorLocation)
+gl.vertexAttribPointer(colorLocation, 1, gl.UNSIGNED_BYTE, true, 0, 0)
+
+const positionCubeBuffer = gl.createBuffer()
+gl.bindBuffer(gl.ARRAY_BUFFER, positionCubeBuffer)
 gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(getCubeGeometry()), gl.STATIC_DRAW)
 gl.enableVertexAttribArray(positionLocation)
 gl.vertexAttribPointer(positionLocation, 3, gl.FLOAT, false, 0, 0)
 
-// For ground geometry
-// gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer)
-gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(getGroundGeometry()), gl.STATIC_DRAW)
-// gl.enableVertexAttribArray(positionLocation)
-// gl.vertexAttribPointer(positionLocation, 3, gl.FLOAT, false, 0, 0)
-
-const colorBuffer = gl.createBuffer()
-gl.bindBuffer(gl.ARRAY_BUFFER, colorBuffer)
-setColors(gl)
+const colorCubeBuffer = gl.createBuffer()
+gl.bindBuffer(gl.ARRAY_BUFFER, colorCubeBuffer)
+setColors(gl, 36, 13)
 gl.enableVertexAttribArray(colorLocation)
-gl.vertexAttribPointer(colorLocation, 2, gl.UNSIGNED_BYTE, true, 2, 2)
+gl.vertexAttribPointer(colorLocation, 1, gl.UNSIGNED_BYTE, true, 0, 0)
+
+const positionTowerBuffer = gl.createBuffer()
+gl.bindBuffer(gl.ARRAY_BUFFER, positionTowerBuffer)
+gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(getTowerGeometry()), gl.STATIC_DRAW)
+gl.enableVertexAttribArray(positionLocation)
+gl.vertexAttribPointer(positionLocation, 3, gl.FLOAT, false, 0, 0)
+
+const colorTowerBuffer = gl.createBuffer()
+gl.bindBuffer(gl.ARRAY_BUFFER, colorTowerBuffer)
+setColors(gl, 36, 15)
+gl.enableVertexAttribArray(colorLocation)
+gl.vertexAttribPointer(colorLocation, 1, gl.UNSIGNED_BYTE, true, 0, 0)
+
+const positionPyramideBuffer = gl.createBuffer()
+gl.bindBuffer(gl.ARRAY_BUFFER, positionPyramideBuffer)
+gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(getPyramideGeometry()), gl.STATIC_DRAW)
+gl.enableVertexAttribArray(positionLocation)
+gl.vertexAttribPointer(positionLocation, 3, gl.FLOAT, false, 0, 0)
+
+const colorPyramideBuffer = gl.createBuffer()
+gl.bindBuffer(gl.ARRAY_BUFFER, colorPyramideBuffer)
+setColors(gl, 18, 20)
+gl.enableVertexAttribArray(colorLocation)
+gl.vertexAttribPointer(colorLocation, 3, gl.UNSIGNED_BYTE, true, 0, 0)
+
+// const positionBuffer = gl.createBuffer()
+// gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer)
+// gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(tetrahedronVertices), gl.STATIC_DRAW)
+
+// // Création du buffer de couleurs
+// const colorBuffer = gl.createBuffer()
+// gl.bindBuffer(gl.ARRAY_BUFFER, colorBuffer)
+// gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(tetrahedronColors), gl.STATIC_DRAW)
 
 //====================================
 // Dessin de l'image
@@ -90,11 +132,6 @@ gl.useProgram(prg)
 
 // Données constantes pour toutes les images de l'animation
 
-// La matrice de projection définie comme produit
-// de la matrice de conversion ndc et la matrice de perspective.
-// fov et zNear sont donnés, ce qui impose h, puis w pour respecter
-//  les proportions de l'image affichée.
-// zfar est à choisir pour inclure les objets à rendre.
 const fieldOfViewInRadians = degToRad(60)
 const zNear = 3 //1
 const zFar = 1000 //2000;
@@ -113,8 +150,8 @@ gl.uniformMatrix4fv(projectionMatrixLocation, false, math.flatten(math.transpose
 const cameraUP = degToRad(0)
 let cameraMatrix = zRotation(cameraUP)
 //    visée : position+orientation
-const radius = 300 // la position de la caméra dépend du rayon du F-henge
-const cameraAngleRadians = degToRad(-3)
+const radius = 320 // la position de la caméra dépend du rayon du F-henge
+const cameraAngleRadians = degToRad(-40)
 let cameraMatrixInit = translation(0, 0, radius * 2.0)
 cameraMatrixInit = math.multiply(xRotation(cameraAngleRadians), cameraMatrixInit)
 
@@ -142,55 +179,89 @@ function drawScene() {
   gl.bindVertexArray(vao)
 
   // Mise à jour des uniforms (et attributes)
-  pos = pos + 1
+  pos = pos + 0.1
   cameraPosRadians = degToRad(pos)
   cameraMatrix = math.multiply(yRotation(cameraPosRadians), cameraMatrixInit)
 
-  //  - en déduire la matrice de vue pour un objet centrée dans la scène
-  const inversedCameraMatrixWorld = math.inv(cameraMatrix)
-
   // Les objets sont numFs copies d'un F sur le cercle de rayon radius
-  // const numFs = 2
-  // for (let ii = 0; ii < numFs; ++ii) {
-  //   // - définir la matrice de positionnement de l'objet dans la scène
-  //   const angle = ii * Math.PI * 2 / numFs;
-  //   const x = Math.cos(angle) * radius;
-  //   const y = Math.sin(angle) * radius;
-  // let objectMatrixWorld = translation(x, 0, -y);
-  // objectMatrixWorld = math.multiply(objectMatrixWorld,yRotation(-0.5*Math.PI+angle));
-  let objectMatrixWorld = math.identity(4)
-  //  - en déduire la matrice de vue de la scène
-  const modelViewMatrix = math.multiply(inversedCameraMatrixWorld, objectMatrixWorld)
-  // In GLSL matrices are transposed!!!
-  gl.uniformMatrix4fv(modelViewMatrixLocation, false, math.flatten(math.transpose(modelViewMatrix)).valueOf())
+  // ...
 
-  const primitiveType = gl.TRIANGLES
-  const offset = 0
-  const count = 111111 // 16 tuiles de 2 triangles avec 3 vertices chacun
-  gl.drawArrays(primitiveType, offset, count)
-  // }
+  drawObjects(positionGroundBuffer, colorGroundBuffer, translation(0, 0, 0), 6)
+
+  drawObjects(positionTowerBuffer, colorTowerBuffer, translation(0, 0, 0), 36)
+
+  // drawObjects(positionPyramideBuffer, colorPyramideBuffer, translation(-180, 45, 67), 12)
+  // Mettre une pyramide dans chaque coin
+  drawObjects(positionPyramideBuffer, colorPyramideBuffer, translation(-200, 45, 200), 12)
+  drawObjects(positionPyramideBuffer, colorPyramideBuffer, translation(200, 45, 200), 12)
+  drawObjects(positionPyramideBuffer, colorPyramideBuffer, translation(-200, 45, -200), 12)
+  drawObjects(positionPyramideBuffer, colorPyramideBuffer, translation(200, 45, -200), 12)
+
+  drawObjects(positionCubeBuffer, colorCubeBuffer, translation(130, 30, 100), 36)
+  drawObjects(positionCubeBuffer, colorCubeBuffer, translation(-120, 30, -100), 36)
+  drawObjects(positionCubeBuffer, colorCubeBuffer, translation(130, 30, -100), 36)
+  drawObjects(positionCubeBuffer, colorCubeBuffer, translation(-120, 30, 100), 36)
+
+  // ...
+
+  flyingCubes()
 
   requestAnimationFrame(drawScene)
 }
 
-//=========================================================
-// Library: to fill buffers with data (geometry or colours)
-//=========================================================
-function setColors(gl) {
-  let myColors = []
-  let cIdx = 16
-  // 3 tuiles de 2 triangles pour la face avant
-  for (let ii = 0; ii < 9; ii++) myColors = myColors.concat([ui8Colors[cIdx], ui8Colors[cIdx + 1], ui8Colors[cIdx + 2]])
+function flyingCubes() {
+  const inversedCameraMatrixWorld = math.inv(cameraMatrix)
 
-  // 3 tuiles de 2 triangles pour la face arrière
-  cIdx += 4
-  for (let ii = 0; ii < 18; ii++) myColors = myColors.concat([ui8Colors[cIdx], ui8Colors[cIdx + 1], ui8Colors[cIdx + 2]])
+  const numFs = 10
+  const rotationSpeed = 0.3 // Taux de rotation autour de l'axe Y
 
-  // 1 tuile de 2 triangles pour les 10 autres faces
-  for (let f = 0; f < 10; f++) {
-    cIdx += 4
-    for (let ii = 0; ii < 5; ii++) myColors = myColors.concat([ui8Colors[cIdx], ui8Colors[cIdx + 1], ui8Colors[cIdx + 2]])
+  for (let ii = 0; ii < numFs; ++ii) {
+    const angle = (ii * Math.PI * 2) / numFs
+
+    const x = Math.cos(angle) * radius
+    const y = Math.sin(angle) * radius
+
+    let objectMatrixWorld = translation(x, 150, -y)
+
+    // Rotation autour de l'axe Y de la tour
+    objectMatrixWorld = math.multiply(yRotation(angle + rotationSpeed * pos, 0), objectMatrixWorld)
+
+    const modelViewMatrix = math.multiply(inversedCameraMatrixWorld, objectMatrixWorld)
+
+    gl.uniformMatrix4fv(modelViewMatrixLocation, false, math.flatten(math.transpose(modelViewMatrix)).valueOf())
+
+    const primitiveType = gl.TRIANGLES
+    const offset = 0
+    const count = 36 // 16 tuiles de 2 triangles avec 3 vertices chacun
+    gl.drawArrays(primitiveType, offset, count)
   }
+}
+
+function drawObjects(positionBuffer, colorBuffer, modelMatrix, count) {
+  const inversedCameraMatrixWorld = math.inv(cameraMatrix)
+
+  gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer)
+  gl.enableVertexAttribArray(positionLocation)
+  gl.vertexAttribPointer(positionLocation, 3, gl.FLOAT, false, 0, 0)
+
+  gl.bindBuffer(gl.ARRAY_BUFFER, colorBuffer)
+  gl.enableVertexAttribArray(colorLocation)
+  gl.vertexAttribPointer(colorLocation, 3, gl.UNSIGNED_BYTE, true, 0, 0)
+
+  const modelViewMatrix = math.multiply(inversedCameraMatrixWorld, modelMatrix)
+
+  gl.uniformMatrix4fv(modelViewMatrixLocation, false, math.flatten(math.transpose(modelViewMatrix)).valueOf())
+
+  const primitiveType = gl.TRIANGLES
+  const offset = 0
+  gl.drawArrays(primitiveType, offset, count)
+}
+
+function setColors(gl, count, color) {
+  let myColors = []
+  let cIdx = color // 3 tuiles de 2 triangles pour la face avant
+
+  for (let ii = 0; ii < count; ii++) myColors = myColors.concat([ui8Colors[cIdx], ui8Colors[cIdx + 1], ui8Colors[cIdx + 2]])
 
   gl.bufferData(gl.ARRAY_BUFFER, new Uint8Array(myColors), gl.STATIC_DRAW)
 }
